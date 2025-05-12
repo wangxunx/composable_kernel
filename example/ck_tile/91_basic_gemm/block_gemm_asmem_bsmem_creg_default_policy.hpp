add95a52f34fd0bd3a8b54c6e604e0ddf85f9708
Xun Wang <xunwang2@amd.com>
2025-05-12 07:44:54 -0500

    add mfma m32 n32 k16 gemm setting
    Signed-off-by: Xun Wang <xunwang2@amd.com>

--- example/ck_tile/91_basic_gemm/block_gemm_asmem_bsmem_creg_default_policy.hpp
+++ example/ck_tile/91_basic_gemm/block_gemm_asmem_bsmem_creg_default_policy.hpp
@@ -28,7 +28,25 @@ struct BlockGemmASmemBSmemCRegDefaultPolicy
         {
             return make_tuple(WarpGemmMfmaBf16Bf16F32M32N32K8TransposedCDistribution{}, 4, 1);
         }
+#elif defined(USING_MFMA_32x32x_8x2)
+#pragma message ("mfma m32 n32 k16")
+        if constexpr(std::is_same_v<typename Problem::ADataType, half_t> &&
+                     std::is_same_v<typename Problem::BDataType, half_t> &&
+                     std::is_same_v<typename Problem::CDataType, float>)
+        {
+            return make_tuple(WarpGemmMfmaF16F16F32M32N32K16TransposedCDistribution{}, 4, 1);
+        }
+        else if constexpr(std::is_same_v<typename Problem::ADataType, bf16_t> &&
+                          std::is_same_v<typename Problem::BDataType, bf16_t> &&
+                          std::is_same_v<typename Problem::CDataType, float>)
+        {
+            return make_tuple(WarpGemmMfmaBf16Bf16F32M32N32K16TransposedCDistribution{}, 4, 1);
+        }
 #endif
+        else
+        {
+            static_assert(false, "Unsupported data type configuration for GEMM warp execution.");
+        }
     }
 };
 
